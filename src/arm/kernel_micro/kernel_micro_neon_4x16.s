07c4f5c60981c70b6249c141de455fdf9c2a8d01
ckswjd99 <ckswjd99@snu.ac.kr>
2023-04-04 18:53:58 +0900

    asm kernel_4x16 un-pipelined

--- src/arm/kernel_micro/kernel_micro_neon_4x16.s
+++ src/arm/kernel_micro/kernel_micro_neon_4x16.s
@@ -56,31 +56,33 @@ sgemm_micro_kernel_neon_4x16:
 // poB .req x2
 // poC .req x3
 // incRowC .req w4
-// incColC .req w5
+incColC .req w5
 iter 	.req w6
 tempA 	.req x7
 tempB 	.req x8
 tempC 	.req x9
 kc_w	.req w10
 kc_x	.req x10
+alpha_vec .req v10.4S
 
 
 .LFB0:
 	.cfi_startproc
 // prepare space for arguments
-	sub	sp, sp, #64
-	.cfi_def_cfa_offset 64
-	str	w0, [sp, 44]
-	str	s0, [sp, 40]
+	// sub	sp, sp, #64
+	// .cfi_def_cfa_offset 64
+	// str	w0, [sp, 44]
+	// str	s0, [sp, 40]
 	// str	x1, [sp, 32]
 	// str	x2, [sp, 24]
-	str	s1, [sp, 20]
-	str	x3, [sp, 8]
-	str	w4, [sp, 16]
-	str	w5, [sp, 4]
+	// str	s1, [sp, 20]
+	// str	x3, [sp, 8]
+	// str	w4, [sp, 16]
+	// str	w5, [sp, 4]
+
 // load C values
 	mov tempC, x3				// tempC = C
-	lsl	w5, w5, 2
+	lsl	incColC, incColC, 2
 	ld1 {v16.4S-v19.4S}, [tempC], x5
 	ld1 {v20.4S-v23.4S}, [tempC], x5
 	ld1 {v24.4S-v27.4S}, [tempC], x5
@@ -90,8 +92,8 @@ kc_x	.req x10
 	mov iter, wzr				// init: iter = 0
 	mov tempA, x1				// tempA = A
 	mov tempB, x2				// tempB = B
-	mov kc_x, x0					// kc on reg x0
-	ldr	w2, [sp, 40]			// load alpha
+	mov kc_x, x0				// kc on reg x0
+	mov w2, v0.s[0]				// load alpha
 	dup v10.4S, w2				// v10.4S = alpha
 	b	.LoopConditionCheck
 
@@ -101,6 +103,7 @@ kc_x	.req x10
 	ld1 {v4.4S-v7.4S}, [tempB], #64
 	fmul v8.4S, v8.4S, v10.4S
 	fmul v9.4S, v9.4S, v10.4S
+
 	fmla v16.4S, v0.4S, v8.4S[0]
 	fmla v17.4S, v1.4S, v8.4S[0]
 	fmla v18.4S, v2.4S, v8.4S[0]
@@ -117,6 +120,7 @@ kc_x	.req x10
 	fmla v29.4S, v1.4S, v8.4S[3]
 	fmla v30.4S, v2.4S, v8.4S[3]
 	fmla v31.4S, v3.4S, v8.4S[3]
+
 	fmla v16.4S, v4.4S, v9.4S[0]
 	fmla v17.4S, v5.4S, v9.4S[0]
 	fmla v18.4S, v6.4S, v9.4S[0]
@@ -134,16 +138,9 @@ kc_x	.req x10
 	fmla v30.4S, v6.4S, v9.4S[3]
 	fmla v31.4S, v7.4S, v9.4S[3]
 	
-// 0 "" 2
-#NO_APP
-	
-	ldr	w2, [sp, 40]
-#APP
-// 94 "kernel_micro_neon_4x16.c" 1
 	ld1 {v8.4S-v9.4S}, [tempA], #32
 	ld1 {v0.4S-v3.4S}, [tempB], #64
 	ld1 {v4.4S-v7.4S}, [tempB], #64
-	dup v10.4S, w2
 	fmul v8.4S, v8.4S, v10.4S
 	fmul v9.4S, v9.4S, v10.4S
 	fmla v16.4S, v0.4S, v8.4S[0]
@@ -179,16 +176,10 @@ kc_x	.req x10
 	fmla v30.4S, v6.4S, v9.4S[3]
 	fmla v31.4S, v7.4S, v9.4S[3]
 	
-// 0 "" 2
-#NO_APP
-		
-	ldr	w2, [sp, 40]
-#APP
-// 97 "kernel_micro_neon_4x16.c" 1
 	ld1 {v8.4S-v9.4S}, [tempA], #32
 	ld1 {v0.4S-v3.4S}, [tempB], #64
 	ld1 {v4.4S-v7.4S}, [tempB], #64
-	dup v10.4S, w2
+	
 	fmul v8.4S, v8.4S, v10.4S
 	fmul v9.4S, v9.4S, v10.4S
 	fmla v16.4S, v0.4S, v8.4S[0]
@@ -224,16 +215,10 @@ kc_x	.req x10
 	fmla v30.4S, v6.4S, v9.4S[3]
 	fmla v31.4S, v7.4S, v9.4S[3]
 	
-// 0 "" 2
-#NO_APP
-	
-	ldr	w2, [sp, 40]
-#APP
-// 100 "kernel_micro_neon_4x16.c" 1
 	ld1 {v8.4S-v9.4S}, [tempA], #32
 	ld1 {v0.4S-v3.4S}, [tempB], #64
 	ld1 {v4.4S-v7.4S}, [tempB], #64
-	dup v10.4S, w2
+	
 	fmul v8.4S, v8.4S, v10.4S
 	fmul v9.4S, v9.4S, v10.4S
 	fmla v16.4S, v0.4S, v8.4S[0]
@@ -269,9 +254,9 @@ kc_x	.req x10
 	fmla v30.4S, v6.4S, v9.4S[3]
 	fmla v31.4S, v7.4S, v9.4S[3]
 	
-// 0 "" 2
-#NO_APP
-	add	iter, iter,	 1	// iter++ at for loop
+	// iter++ at for loop
+	add	iter, iter,	 1	
+
 .LoopConditionCheck:
 	add	w1, kc_w, 7
 	cmp	kc_w, 0
@@ -289,8 +274,8 @@ kc_x	.req x10
 	st1 {v28.4S-v31.4S}, [tempC], x5
 
 // END FUNCTION	
-	add	sp, sp, 64
-	.cfi_def_cfa_offset 0
+	// add	sp, sp, 64
+	// .cfi_def_cfa_offset 0
 	ret
 	.cfi_endproc
 .LFE0:
