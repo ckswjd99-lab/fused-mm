69ac6b18124e9d0d707cd61042e4e1749cb6dd4e
ckswjd99 <ckswjd99@snu.ac.kr>
2023-04-04 18:00:54 +0900

    asm kernel_4x16

--- src/arm/kernel_micro/kernel_micro_neon_4x16.c
+++ src/arm/kernel_micro/kernel_micro_neon_4x16.c
@@ -1,5 +1,63 @@
 #include "kernel_micro.h"
 
+#define FMLA(A, B, alpha) __asm__ volatile(         \
+    /********** K=0 **********/                     \
+                                                    \
+    "ld1 {v8.4S-v9.4S}, [%[A]], #32\n\t"            \
+                                                    \
+    "ld1 {v0.4S-v3.4S}, [%[B]], #64\n\t"            \
+    "ld1 {v4.4S-v7.4S}, [%[B]], #64\n\t"            \
+                                                    \
+    /* mult alpha to A */                           \
+    "dup v10.4S, %w[alpha]\n\t"                     \
+    "fmul v8.4S, v8.4S, v10.4S\n\t"                 \
+    "fmul v9.4S, v9.4S, v10.4S\n\t"                 \
+                                                    \
+    /* MAC: C += alpha_A @ B */                     \
+    "fmla v16.4S, v0.4S, v8.4S[0]\n\t"              \
+    "fmla v17.4S, v1.4S, v8.4S[0]\n\t"              \
+    "fmla v18.4S, v2.4S, v8.4S[0]\n\t"              \
+    "fmla v19.4S, v3.4S, v8.4S[0]\n\t"              \
+                                                    \
+    "fmla v20.4S, v0.4S, v8.4S[1]\n\t"              \
+    "fmla v21.4S, v1.4S, v8.4S[1]\n\t"              \
+    "fmla v22.4S, v2.4S, v8.4S[1]\n\t"              \
+    "fmla v23.4S, v3.4S, v8.4S[1]\n\t"              \
+                                                    \
+    "fmla v24.4S, v0.4S, v8.4S[2]\n\t"              \
+    "fmla v25.4S, v1.4S, v8.4S[2]\n\t"              \
+    "fmla v26.4S, v2.4S, v8.4S[2]\n\t"              \
+    "fmla v27.4S, v3.4S, v8.4S[2]\n\t"              \
+                                                    \
+    "fmla v28.4S, v0.4S, v8.4S[3]\n\t"              \
+    "fmla v29.4S, v1.4S, v8.4S[3]\n\t"              \
+    "fmla v30.4S, v2.4S, v8.4S[3]\n\t"              \
+    "fmla v31.4S, v3.4S, v8.4S[3]\n\t"              \
+                                                    \
+    "fmla v16.4S, v4.4S, v9.4S[0]\n\t"              \
+    "fmla v17.4S, v5.4S, v9.4S[0]\n\t"              \
+    "fmla v18.4S, v6.4S, v9.4S[0]\n\t"              \
+    "fmla v19.4S, v7.4S, v9.4S[0]\n\t"              \
+                                                    \
+    "fmla v20.4S, v4.4S, v9.4S[1]\n\t"              \
+    "fmla v21.4S, v5.4S, v9.4S[1]\n\t"              \
+    "fmla v22.4S, v6.4S, v9.4S[1]\n\t"              \
+    "fmla v23.4S, v7.4S, v9.4S[1]\n\t"              \
+                                                    \
+    "fmla v24.4S, v4.4S, v9.4S[2]\n\t"              \
+    "fmla v25.4S, v5.4S, v9.4S[2]\n\t"              \
+    "fmla v26.4S, v6.4S, v9.4S[2]\n\t"              \
+    "fmla v27.4S, v7.4S, v9.4S[2]\n\t"              \
+                                                    \
+    "fmla v28.4S, v4.4S, v9.4S[3]\n\t"              \
+    "fmla v29.4S, v5.4S, v9.4S[3]\n\t"              \
+    "fmla v30.4S, v6.4S, v9.4S[3]\n\t"              \
+    "fmla v31.4S, v7.4S, v9.4S[3]\n\t"              \
+                                                    \
+    ::[A] "r"(A), [B] "r"(B), [alpha] "r"(alpha)    \
+)
+
+
 void sgemm_micro_kernel_neon_4x16(
     int kc, float alpha, const float *A, const float *B, float beta, float *C, int incRowC, int incColC)
 {
@@ -28,67 +86,18 @@ void sgemm_micro_kernel_neon_4x16(
     );
 
     // COMPUTE C <- C + alpha * AB
-    for (k = 0; k < kc/2; k++)
+    for (k = 0; k < kc/8; k++)
     {
-        // compute (8, 1) @ (1, 8)
-        __asm__ volatile(
-            /********** K=0 **********/
-
-            // load A(4, 2) & B(2, 16)
-            "ld1 {v8.4S-v9.4S}, [%[A]], #32\n\t"
-
-            "ld1 {v0.4S-v3.4S}, [%[B]], #64\n\t"
-            "ld1 {v4.4S-v7.4S}, [%[B]], #64\n\t"
-
-            // mult alpha to A
-            "dup v10.4S, %w[alpha]\n\t"
-            "fmul v8.4S, v8.4S, v10.4S\n\t"
-            "fmul v9.4S, v9.4S, v10.4S\n\t"
-
-            // MAC: C += alpha_A @ B
-            "fmla v16.4S, v0.4S, v8.4S[0]\n\t"
-            "fmla v17.4S, v1.4S, v8.4S[0]\n\t"
-            "fmla v18.4S, v2.4S, v8.4S[0]\n\t"
-            "fmla v19.4S, v3.4S, v8.4S[0]\n\t"
-
-            "fmla v20.4S, v0.4S, v8.4S[1]\n\t"
-            "fmla v21.4S, v1.4S, v8.4S[1]\n\t"
-            "fmla v22.4S, v2.4S, v8.4S[1]\n\t"
-            "fmla v23.4S, v3.4S, v8.4S[1]\n\t"
-
-            "fmla v24.4S, v0.4S, v8.4S[2]\n\t"
-            "fmla v25.4S, v1.4S, v8.4S[2]\n\t"
-            "fmla v26.4S, v2.4S, v8.4S[2]\n\t"
-            "fmla v27.4S, v3.4S, v8.4S[2]\n\t"
-
-            "fmla v28.4S, v0.4S, v8.4S[3]\n\t"
-            "fmla v29.4S, v1.4S, v8.4S[3]\n\t"
-            "fmla v30.4S, v2.4S, v8.4S[3]\n\t"
-            "fmla v31.4S, v3.4S, v8.4S[3]\n\t"
-
-            "fmla v16.4S, v4.4S, v9.4S[0]\n\t"
-            "fmla v17.4S, v5.4S, v9.4S[0]\n\t"
-            "fmla v18.4S, v6.4S, v9.4S[0]\n\t"
-            "fmla v19.4S, v7.4S, v9.4S[0]\n\t"
-
-            "fmla v20.4S, v4.4S, v9.4S[1]\n\t"
-            "fmla v21.4S, v5.4S, v9.4S[1]\n\t"
-            "fmla v22.4S, v6.4S, v9.4S[1]\n\t"
-            "fmla v23.4S, v7.4S, v9.4S[1]\n\t"
-
-            "fmla v24.4S, v4.4S, v9.4S[2]\n\t"
-            "fmla v25.4S, v5.4S, v9.4S[2]\n\t"
-            "fmla v26.4S, v6.4S, v9.4S[2]\n\t"
-            "fmla v27.4S, v7.4S, v9.4S[2]\n\t"
-
-            "fmla v28.4S, v4.4S, v9.4S[3]\n\t"
-            "fmla v29.4S, v5.4S, v9.4S[3]\n\t"
-            "fmla v30.4S, v6.4S, v9.4S[3]\n\t"
-            "fmla v31.4S, v7.4S, v9.4S[3]\n\t"
-
-            ::[A] "r"(A), [B] "r"(B), [alpha] "r"(alpha)
-        );
-        
+        FMLA(A, B, alpha);
+        A += 4*2;
+        B += 16*2;
+        FMLA(A, B, alpha);
+        A += 4*2;
+        B += 16*2;
+        FMLA(A, B, alpha);
+        A += 4*2;
+        B += 16*2;
+        FMLA(A, B, alpha);
         A += 4*2;
         B += 16*2;
     }
